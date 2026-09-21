8124ec07ae98b9941b6023a567ff5952fd322709
Yunus Emre Sirin <93075567+yunusemresirin@users.noreply.github.com>
2026-09-21 21:32:07 +0200

    test: use typed configuration variants

--- services/configuration-service/src/test/java/de/hbrs/seka/wirschiffendas/configuration/application/ConfigurationApplicationServiceTest.java
+++ services/configuration-service/src/test/java/de/hbrs/seka/wirschiffendas/configuration/application/ConfigurationApplicationServiceTest.java
@@ -1,6 +1,7 @@
 package de.hbrs.seka.wirschiffendas.configuration.application;
 
 import de.hbrs.seka.wirschiffendas.configuration.api.CreateConfigurationRequest;
+import de.hbrs.seka.wirschiffendas.configuration.domain.ConfigurationVariant;
 import de.hbrs.seka.wirschiffendas.configuration.domain.EngineConfiguration;
 import de.hbrs.seka.wirschiffendas.configuration.infrastructure.EngineConfigurationRepository;
 import org.junit.jupiter.api.Test;
@@ -29,11 +30,11 @@ class ConfigurationApplicationServiceTest {
                 .thenAnswer(invocation -> invocation.getArgument(0));
 
         CreateConfigurationRequest request = new CreateConfigurationRequest(
-                "STANDARD",
-                "PREMIUM",
-                "STANDARD",
-                "PREMIUM",
-                "ADVANCED");
+                ConfigurationVariant.STANDARD,
+                ConfigurationVariant.PREMIUM,
+                ConfigurationVariant.STANDARD,
+                ConfigurationVariant.PREMIUM,
+                ConfigurationVariant.ADVANCED);
 
         EngineConfiguration created = service.create(request);
 
